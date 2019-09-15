1fcf2eb8b0ddad14c20ee30a6299530461e285b3
Miles <milescenabeverly@aol.com>
2019-09-15 17:51:28 -0600

    made quick fix I had forgotten to implement in utils.py

--- HW1/utils.py
+++ HW1/utils.py
@@ -79,10 +79,11 @@ def decimalToBinary(deci):
 
 def binaryToDecimal(binary):
     decimal, i = 0, 0
-    while binary != 0:
-        dec = binary % 10
+    binary2 = int(binary)
+    while binary2 != 0:
+        dec = binary2 % 10
         decimal = decimal + (dec * pow(2,i))
-        binary = binary // 10
+        binary2 = binary2 // 10
         i += 1
     return decimal
 
