8a4d0753386f19d7ac60fb20ba8bdefd41fdbba6
Miles <milescenabeverly@aol.com>
2019-10-02 11:33:14 -0600

    created prime generation functions. I made two variants, a deterministic prime generation function and a probabilistic generation function, which the user can manual select between

--- HW2/task4/rsa-utils.py
+++ HW2/task4/rsa-utils.py
@@ -1,4 +1,4 @@
-from random import randrange, getrandbits
+from random import randint, randrange, getrandbits
 
 #
 # Helper function called by generatePrime() to check for primality.uses Miller-Rabin test,
@@ -45,7 +45,9 @@ def generatePotentialPrime():
     return p
 
 #
-# Generates a number which is almost certaintly prime.
+# Generates a number which is almost certaintly prime. The probability of the number not
+# being prime is significantly smaller than that of program failure due to a hardware
+# malfunction
 #
 def generatePrime():
     # Generate potential prime number
@@ -54,3 +56,55 @@ def generatePrime():
     while not isPrime(n):
         n = generatePotentialPrime()
     return n
+
+#
+# This function generates a prime number using a deterministic variant of the Miller Test
+# which limits the size of the numbers it can generate, but ensures with 100% certainty that
+# the number is prime. More information about the deterministic version of the Miller-Rabin
+# test can be found at:
+# https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Deterministic_variants
+#  
+def isDefinitelyPrime(n):
+    tests = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+    # 2 and 3 are prime
+    if n == 2 or n == 3:
+        return True
+    if n <= 1 or n % 2 == 0:
+        return False
+    # find d and r
+    r = 0
+    d = n - 1
+    while d % 2 == 0:
+        r += 1
+        d //= 2
+    for a in tests:
+        x = pow(a, d, n)
+        if x != 1 and x != n - 1:
+            j = 1
+            while j < r and x != n - 1:
+                x = pow(x, 2, n)
+                if x == 1:
+                    return False
+                j += 1
+            if x != n - 1:
+                return False
+    return True
+
+def generateDeterministicPrime():
+    # generate a random number from 2 to 3317044064679887385961813. This number was chosen
+    # because it is the largest prime number less than 3317044064679887385961981, which is
+    # the smallest composite number for which the above isDefPrime function will return a
+    # false positive. This was determined by running the commented-out script below this
+    # function. After generating the number, while it is not prime, add 1 to number
+    n = randint(2, 3317044064679887385961813)
+    while not isDefinitelyPrime(n):
+        n += 1
+    return n
+
+# This script was used to determine the largest prime less than 3317044064679887385961981
+"""
+n = 3317044064679887385961980
+while not isDefinitelyPrime(n):
+    n -= 1
+print(n)
+"""
