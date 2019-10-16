b990ca88586aca5328738fbe58e59e53520d3e77
Miles <milescenabeverly@aol.com>
2019-10-16 16:57:17 -0600

    added decimal to binary function

--- HW3/utils.py
+++ HW3/utils.py
@@ -6,3 +6,21 @@ def sum_list(array):
     for item in array:
         sum += int(item)
     return sum
+
+
+def decimalToBinary(deci):
+    converted = bin(deci).replace('0b', '')
+    while len(converted) < 8:
+        converted = '0' + converted
+    return converted
+
+
+def binaryToDecimal(binary):
+    decimal, i = 0, 0
+    binary2 = int(binary)
+    while binary2 != 0:
+        dec = binary2 % 10
+        decimal = decimal + (dec * pow(2,i))
+        binary2 = binary2 // 10
+        i += 1
+    return decimal
