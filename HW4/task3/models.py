587399807d6f14c83149e080bd1c3a9ae1e6d292
Miles <milescenabeverly@aol.com>
2019-11-01 15:17:27 -0600

    Made database model for user and made directories/files for hashed passwords and salt

--- HW4/task3/models.py
+++ HW4/task3/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    birth_date = models.DateField()
+    street = models.CharField(max_length=50)
+    apt = models.CharField(max_length=2)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=15)
+    zip = models.PositiveIntegerField()
+
+    def signup(self):
+        # Add validation from task 1 and 2 here before creating new user
+
+        # If password is validated successfully, save user in database and send password 
+        # to password file
+
+        self.save()
