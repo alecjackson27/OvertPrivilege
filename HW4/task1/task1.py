d1d7377e36e9d3c917f0fa112d09d58756a97b5c
Miles <milescenabeverly@aol.com>
2019-11-01 18:55:36 -0600

    Added list of thirty common dictionary words to tas1.py and updated to-do list

---
+++ HW4/task1/task1.py
@@ -0,0 +1,9 @@
+# The list of thirty common dictionary words used in passwords. List is taken from the following links:
+# https://www.passwordrandom.com/most-popular-passwords
+# https://www.passwordrandom.com/most-popular-passwords
+COMMON_WORDS = [
+    'pass', 'word', 'love', 'cowboy', 'yankee', 'lake', 'monkey', 'dragon', 'sunshine',
+    'princess', 'football', 'welcome', 'master', 'freedom', 'login', 'star', 'what', 'ever',
+    'hello', 'flower', 'baseball', 'mustang', 'shadow', 'access', 'trust', 'jesus', 'ninja',
+    'super', 'ranger', 'soccer'
+]
