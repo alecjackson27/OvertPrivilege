cb75b0bb8531efc163fcf5f37c3c6de666921d75
Miles <milescenabeverly@aol.com>
2019-11-02 00:05:20 -0600

    Added salt to User model

--- HW4/task3/models.py
+++ HW4/task3/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from .utils import *
 
 # Create your models here.
 
@@ -13,5 +14,6 @@ class User(models.Model):
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=15)
     zip = models.PositiveIntegerField()
+    salt = models.CharField(max_length=16, default=generate_salt)
     failed_logins = models.IntegerField(default=0)
     locked_out_until = models.DateTimeField(default=timezone.now)
