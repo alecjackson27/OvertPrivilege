fb200fb29f07f374ec957b85524471efbcc2f570
Adam-Lundell <adamlund49@yahoo.com>
2019-11-08 16:57:50 -0700

    made the needed function triple check that it writes to the correct file path

--- HW4/task3/utils.py
+++ HW4/task3/utils.py
@@ -2,6 +2,7 @@
 # password and salt to their respective files
 
 import random
+import hashlib
 
 def generate_salt():
     ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -10,6 +11,19 @@ def generate_salt():
         salt += random.choice(ALPHABET)
     return salt
 
+# concatonate the salt to the password
+def calculate_hash(password, salt, userID):
+    passSalt = password + str(salt)
+
+    shaw1 = hashlib.sha1()
+    shaw1.update(passSalt.encode('utf-8'))
+    theHash = shaw1.hexdigest()
+    combo = userID + " " + theHash
+    theFile = open("passwords.txt","a") #not sure if it gets to the file or not
+    theFile.write(combo + "\n")
+    theFile.close()
+
+#
 # TO DO: Write function which takes as parameters a password, salt, and user ID number. The function
 # will the calculate the hash of (password + salt) and write the space separated user ID/hash pair
 # to the password.txt file with the format: userId hash. Each row of password.txt should be
