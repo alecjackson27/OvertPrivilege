335da1c00c8f2b1b6e50d776b56622e6bc6b41eb
Miles <milescenabeverly@aol.com>
2019-11-08 17:04:12 -0700

    saving

--- HW4/task3/views.py
+++ HW4/task3/views.py
@@ -14,7 +14,20 @@ def signup(request):
 def create(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
+        print(request.POST)
         # Use form data (contained in request.POST) to validate info using methods from tasks 1 and 2
+        with open('../task1/createdPasswords.txt', 'r') as f:
+            ECLP = f.read().split()
+        dictionary_word = ""
+        for i in range(len(ECLP)):
+            if ECLP[i] =="$":
+                dictionary_word = ECLP[i + 1]
+            elif ECLP[i] == request.POST['password']:
+                # Password is a variation of dictionary_word. Return to signup and inform user
+                print("Placeholder")
+
+        birth_date = request.POST['birth date'][6:8] + "/" + request.POST['birth date'][8:] \
+            + "/" + request.POST['birth date'][:5]
 
         # If data checks out, create a new user with form data and save it in the database, then
         # save hashed password and salt to respective files. Then redirect to a new URL:
