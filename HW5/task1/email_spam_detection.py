c8431a1bf5ea33663edd94f4563bb0719b8223cf
Miles <milescenabeverly@aol.com>
2019-11-27 20:45:25 -0700

    Started task 2

---
+++ HW5/task1/email_spam_detection.py
@@ -0,0 +1,165 @@
+'''
+Citations:
+Code from the article (1) and associated github account (2) below used as a starting point:
+1. https://medium.com/@randerson112358/email-spam-detection-using-python-machine-learning-abe38c889855
+2. https://github.com/randerson112358/Python/blob/master/Email_Spam_Detection/Email_Spam_Detection.ipynb
+
+emails.csv data set from: https://www.kaggle.com/balakishan77/spam-or-ham-email-classification/data
+'''
+
+
+import numpy as np 
+import pandas as pd 
+import nltk
+from nltk.corpus import stopwords
+import string
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report,confusion_matrix, accuracy_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.svm import LinearSVC
+
+df = pd.read_csv('emails.csv')
+
+
+#Check for and remove duplicates
+df.drop_duplicates(inplace = True)
+
+
+#Tokenization (a list of tokens), will be used as the analyzer
+#1.Punctuations are [!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]
+#2.Stop words in natural language processing, are useless words (data).
+def process_text(text):
+    '''
+    What will be covered:
+    1. Remove punctuation
+    2. Remove stopwords
+    3. Return list of clean text words
+    '''
+    
+    #1
+    nopunc = [char for char in text if char not in string.punctuation]
+    nopunc = ''.join(nopunc)
+    
+    #2
+    clean_words = [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
+    
+    #3
+    return clean_words
+
+
+#Convert string to integer counts, learn the vocabulary dictionary and return term-document matrix
+messages_bow = CountVectorizer(analyzer=process_text).fit_transform(df['text'])
+
+
+#Split the data into 80% training (X_train & y_train) and 20% testing (X_test & y_test) data sets
+X_train, X_test, y_train, y_test = train_test_split(messages_bow, df['spam'], test_size = 0.20, random_state = 0)
+
+
+#Create and train the Naive Bayes classifier
+classifier = MultinomialNB()
+classifier.fit(X_train, y_train)
+
+print("NB")
+print()
+
+#Print the predictions
+print(classifier.predict(X_train))
+
+#Print the actual values
+print(y_train.values)
+
+#Evaluate the model on the training data set
+pred = classifier.predict(X_train)
+print(classification_report(y_train ,pred ))
+print('Confusion Matrix: \n',confusion_matrix(y_train,pred))
+print()
+print('Accuracy: ', accuracy_score(y_train,pred))
+
+#Print the predictions
+print('Predicted value: ',classifier.predict(X_test))
+
+#Print Actual Label
+print('Actual value: ',y_test.values)
+
+#Evaluate the model on the test data set
+from sklearn.metrics import classification_report,confusion_matrix, accuracy_score
+pred = classifier.predict(X_test)
+print(classification_report(y_test ,pred ))
+
+print('Confusion Matrix: \n', confusion_matrix(y_test,pred))
+print()
+print('Accuracy: ', accuracy_score(y_test,pred))
+
+
+#Create and train the LinearSVC classifier
+classifier = LinearSVC()
+classifier.fit(X_train, y_train)
+
+print("SVC")
+print()
+
+#Print the predictions
+print(classifier.predict(X_train))
+
+#Print the actual values
+print(y_train.values)
+
+#Evaluate the model on the training data set
+pred = classifier.predict(X_train)
+print(classification_report(y_train ,pred ))
+print('Confusion Matrix: \n',confusion_matrix(y_train,pred))
+print()
+print('Accuracy: ', accuracy_score(y_train,pred))
+
+#Print the predictions
+print('Predicted value: ',classifier.predict(X_test))
+
+#Print Actual Label
+print('Actual value: ',y_test.values)
+
+#Evaluate the model on the test data set
+from sklearn.metrics import classification_report,confusion_matrix, accuracy_score
+pred = classifier.predict(X_test)
+print(classification_report(y_test ,pred ))
+
+print('Confusion Matrix: \n', confusion_matrix(y_test,pred))
+print()
+print('Accuracy: ', accuracy_score(y_test,pred))
+
+
+#Create and train the SGDClassifier
+classifier = SGDClassifier()
+classifier.fit(X_train, y_train)
+
+print("SGD")
+print()
+
+#Print the predictions
+print(classifier.predict(X_train))
+
+#Print the actual values
+print(y_train.values)
+
+#Evaluate the model on the training data set
+pred = classifier.predict(X_train)
+print(classification_report(y_train ,pred ))
+print('Confusion Matrix: \n',confusion_matrix(y_train,pred))
+print()
+print('Accuracy: ', accuracy_score(y_train,pred))
+
+#Print the predictions
+print('Predicted value: ',classifier.predict(X_test))
+
+#Print Actual Label
+print('Actual value: ',y_test.values)
+
+#Evaluate the model on the test data set
+from sklearn.metrics import classification_report,confusion_matrix, accuracy_score
+pred = classifier.predict(X_test)
+print(classification_report(y_test ,pred ))
+
+print('Confusion Matrix: \n', confusion_matrix(y_test,pred))
+print()
+print('Accuracy: ', accuracy_score(y_test,pred))
---
+++ HW5/task1/encrypt.py
@@ -0,0 +1,120 @@
+import sys
+import os
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import *#QMainWindow, QLabel, QGridLayout, QWidget, QPushButton, \
+#    QApplication
+from PyQt5.QtGui import QIcon, QGuiApplication
+from PyQt5.QtCore import QSize
+from PyQt5.QtCore import pyqtSlot
+from RSAutils import encryption
+
+class EncryptWindow(QMainWindow):
+
+    # The function to select the desired file. Will be called when the user
+    # clicks the 'Browse' button
+    @pyqtSlot()
+    def on_click(self):
+        self.textbox.setText(QFileDialog.getOpenFileName(self, "Select File", "", "public.key")[0])
+
+    def encryptClick(self):
+        # Call function to encrypt message.
+        if os.path.exists(self.textbox.text()):
+            self.cipherbox.setText(encryption(self.messagebox.text(), self.textbox.text()))
+        else:
+            self.cipherbox.setText("Invalid file path")
+
+    # The help message box function
+    def helpMethod(self):
+        QMessageBox.about(
+            self,
+            "Help",
+            """This program encrypts a message using a public key.\
+            Type the message in the top textfield and select\
+            your desired public.key file, then click 'Encrypt'""".replace("            ", ' ')
+            )
+
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.setMinimumSize(QSize(400, 240))
+        self.setWindowTitle("RSA Encryption")
+
+        centralWidget = QWidget(self)
+        self.setCentralWidget(centralWidget)
+
+        # The text box for the user's desired file
+        self.textbox = QLineEdit(self)
+        self.textbox.move(100, 20)
+        self.textbox.resize(185, 30)
+        self.textbox.setReadOnly(True)
+
+        # The browse files button
+        self.button = QPushButton('Browse', self)
+        #self.button.setToolTip('This is an example button')
+        self.button.move(290, 20)
+        self.button.clicked.connect(self.on_click)
+
+        # The label for the text box
+        self.textLabel = QLabel(self)
+        self.textLabel.setText('Public Key:')
+        self.textLabel.move(20, 20)
+
+        # The text box for the user's message
+        self.messagebox = QLineEdit(self)
+        self.messagebox.move(100, 60)
+        self.messagebox.resize(185, 30)
+
+        # The label for the message box
+        self.messageLabel = QLabel(self)
+        self.messageLabel.setText('Plain Text:')
+        self.messageLabel.move(20, 60)
+
+        # The encrypt button
+        self.encryptButton = QPushButton('Encrypt', self)
+        self.encryptButton.move(290, 60)
+        self.encryptButton.clicked.connect(self.encryptClick)
+
+        # The text area for the ciphertext
+        self.cipherbox = QTextEdit(self)
+        self.cipherbox. move(100, 100)
+        self.cipherbox.resize(290, 130)
+        self.cipherbox.setReadOnly(True)
+
+        # The label for the ecrypted ciphertext box
+        self.encryptLabel = QLabel(self)
+        self.encryptLabel.setText('Cipher Text:')
+        self.encryptLabel.move(20, 100)
+
+        # The help option on the toolbar
+        helpAct = QAction(QIcon('help.png'), '&Help', self)
+        helpAct.triggered.connect(self.helpMethod)
+
+        # The toolbar
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&Help')
+        fileMenu.addAction(helpAct)
+
+        self.show()
+    
+    # Overwriting the resizeEvent() function so that the proportions of the GUI
+    # remain intact
+    def resizeEvent(self, event):
+        self.button.move(self.width() - 110, 20)
+        self.encryptButton.move(self.width() - 110, 60)
+        self.messagebox.resize(self.width() - 215, 30)
+        self.textbox.resize(self.width() - 215, 30)
+        self.cipherbox.resize(self.width() - 110, self.height() - 110)
+        QMainWindow.resizeEvent(self, event)
+
+    # Keeps the clipboard global on Windows and Mac. Linux requires clipboard manager.
+    def closeEvent(self, event):
+        clipboard = QGuiApplication.clipboard()
+        event = QtCore.QEvent(QtCore.QEvent.Clipboard)
+        QGuiApplication.sendEvent(clipboard, event)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = EncryptWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
+
