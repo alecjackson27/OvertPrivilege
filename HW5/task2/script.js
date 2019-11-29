3ae294952279538065be4e837398e0014ea8b131
Miles <milescenabeverly@aol.com>
2019-11-29 12:56:56 -0700

    Finished tautologies function

--- HW5/task2/script.js
+++ HW5/task2/script.js
@@ -22,6 +22,25 @@ class Scanner {
     }
 
     // tautologies scan
+    tautologies() {
+        if (this.sql.includes('=')) {
+            let indices = [];
+            for(let i=0; i < this.sql.length; i++) {
+                if (str[i] === '=') indices.push(i);
+            }
+            for (let i = 0; i < indices.length; i++) {
+                index = indices[i];
+                let tautologies_flag = false;
+                if (this.sql[i-1] === this.sql[i+1]){
+                    this.score++;
+                    tautologies_flag = true;
+                }
+            }
+            if (tautologies_flag) {
+                this.results.description += 'This SQL likely includes a tautologies attack. ';
+            }
+        }
+    }
 
     // illegal/logically incorrect scan
 
@@ -30,7 +49,7 @@ class Scanner {
     // piggy-back scan
     piggyBack() {
         if (this.sql.includes(';')) {
-            this.results.score += 5; // not actually genuine score, don't know how I want to score things...
+            this.results.score += 1; // not actually genuine score, don't know how I want to score things...
             this.results.description += 'This SQL likely includes a piggy-back attack. ';
         }
     }
