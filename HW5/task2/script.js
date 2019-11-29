be413ba63f5318946bc44bee86f0a34cdc16b3b7
Miles <milescenabeverly@aol.com>
2019-11-29 15:56:10 -0700

    Updated tautologies function so it also reports that inference attack is possible (some of the same indicators of tautology attack can also be indicators of an inference attack

--- HW5/task2/script.js
+++ HW5/task2/script.js
@@ -66,18 +66,18 @@ class Scanner {
             if (this.numEq() > 1) {
                 tautologies_flag = true;
                 this.results.score++;
-                this.results.description += 'This SQL likely includes a tautologies attack because it contains "="';
+                this.results.description += 'This SQL likely includes a tautologies attack or an inference attack because it contains "="';
             }
         } else if (this.numEq() > 2){
             tautologies_flag = true;
             this.results.score++;
-            this.results.description += 'This SQL likely includes a tautologies attack because it contains "="';
+            this.results.description += 'This SQL likely includes a tautologies attack or an inference attack because it contains "="';
         }
         if (this.sql.includes('<')) {
             if (tautologies_flag){
                 this.results.description += ' and it contains "<"';
             } else {
-                this.results.description += 'This SQL likely includes a tautologies attack because it contains "<"';
+                this.results.description += 'This SQL likely includes a tautologies attack or an inference attack because it contains "<"';
             }
             tautologies_flag = true;
             this.results.score++;
@@ -86,7 +86,7 @@ class Scanner {
             if (tautologies_flag){
                 this.results.description += ' and it contains ">"';
             } else {
-                this.results.description += 'This SQL likely includes a tautologies attack because it contains ">"';
+                this.results.description += 'This SQL likely includes a tautologies attack or an inference attack because it contains ">"';
             }
             tautologies_flag = true;
             this.results.score++;
