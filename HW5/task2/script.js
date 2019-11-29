5bdbd456150a1659cb9b9a603f3c10466d45cf1b
Miles <milescenabeverly@aol.com>
2019-11-29 13:44:18 -0700

    Wrote union scan function

--- HW5/task2/script.js
+++ HW5/task2/script.js
@@ -37,6 +37,15 @@ class Scanner {
             this.results.score++;
         }
         if (tautologies_flag) {
+            if (this.sql.includes('--')) {
+                this.results.score++;
+            }
+            if (this.sql.toLower().includes('and')) {
+                this.results.score++;
+            }
+            if (this.sql.toLower().includes('or')) {
+                this.results.score++;
+            }
             this.results.description += 'This SQL likely includes a tautologies attack. ';
         }
     }
@@ -44,11 +53,24 @@ class Scanner {
     // illegal/logically incorrect scan
 
     // union scan
+    union() {
+        if (this.sql.toLower().includes('union')) {
+            this.results.score++;
+            if (this.sql.includes('--')) {
+                this.results.score++;
+            }
+            this.results.description += 'This SQL likely includes a union attack. ';
+        }
+    }
 
     // piggy-back scan
     piggyBack() {
         if (this.sql.includes(';')) {
             this.results.score += 1; // not actually genuine score, don't know how I want to score things...
+            // If the sql also includes "--", it is even more likely to be an attack
+            if (this.sql.includes('--')) {
+                this.results.score += 1;
+            }
             this.results.description += 'This SQL likely includes a piggy-back attack. ';
         }
     }
@@ -67,6 +89,9 @@ class Scanner {
             alternate_encoding_flag = true;
         }
         if (alternate_encoding_flag) {
+            if (this.sql.includes('--')) {
+                this.results.score++;
+            }
             this.results.description += 'This SQL likely includes a alternate encoding attack. ';
         }
     }
